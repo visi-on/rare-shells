e1d68223615bbed372dc5b59bf9550ea9c502cd9
Natalia <adelina09715@gmail.com>
2022-05-30 09:40:14 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
+  <Footer />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 import "./assets/css/main.css";
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer :style="bgImg">
+    <!-- <p><span>O.N</span>atalia</p> -->
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bgImg: {
+        backgroundImage: `url(${require("@/assets/images/meer-strand.png")})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+footer {
+  height: 55vh;
+  /* background-image: url("../assets/images/meer-strand.png"); */
+}
+</style>
